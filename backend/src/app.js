f35d715391b1ab541ce8a27192f5d10505561f6b
wmata141 <wxmm141@gmail.com>
2024-06-05 18:53:14 -0400

    backend first step bd and config

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from "cors";
+
+import welcomeRoutes from './routes/welcomeRoutes.js';
+
+const app = express();
+app.use(cors());
+
+// Configurar middlewares
+app.use(express.json());
+
+// Configurar rutas
+app.use('/', welcomeRoutes);
+
+
+export default app 
---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import app from "./app.js";
+import connectDB from "./db.js";
+import config from './config.js';
+
+// Conectarse a la base de datos de MongoDB
+connectDB();
+
+const port = config.PORT || 8000
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log("Servidor iniciado en el puerto: ",port);
+  console.log("process.env.PORT :",process.env.PORT);
+});
